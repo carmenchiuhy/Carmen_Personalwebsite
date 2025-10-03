ea7b2406487ec943a5ab68976cf76b44b2df5de2
carmenchiuhy <eechiuhy@gmail.com>
2025-10-03 18:26:32 +0800

    button

--- code.js
+++ code.js
@@ -14,7 +14,10 @@ var mycode = document.createElement("div");
 //change basic attributes
 mycode.id = "mywork";
 mycode.innerHTML = "Here is some of my recent work!";
-mycode.style.color = "white";
+mycode.style.color = "black";
+mycode.style.backgroundColor = "white";   
+mycode.style.textAlign = "center";
+mycode.style.cursor = "pointer"; 
 
 //add event listener
 mycode.addEventListener("click", welcometowork);
@@ -24,9 +27,32 @@ document.querySelector("#mywork").appendChild(mycode);
 function welcometowork() {
     //alert("Welcome to my work section!");
 
-    mycode.innerHTML = "I am glad you are here!";
+    mycode.innerHTML = "Thank you for visiting my work section!";
+    mycode.style.backgroundColor = "orange";
+    mycode.style.fontSize = "20px";
 }
 
 
+
+
+
+//try
+
+var one = document.createElement("div");
+//change basic attributes
+one.id = "1";
+one.innerHTML = "Hello!";
+one.style.color = "black";
+one.style.backgroundColor = "white";   
+one.style.textAlign = "center";
+
+//add event listener
+one.addEventListener("click", w);
+
+document.querySelector("#1").appendChild(one);
+
+function w() {
+    alert("Welcome to my work section!");
+}
 
 
